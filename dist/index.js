2ccef491210751d4d857b60a33253937853185ef
Luan Nico <luanpotter27@gmail.com>
2021-05-15 14:06:42 -0400

    .

--- dist/index.js
+++ dist/index.js
@@ -34,9 +34,10 @@ async function run() {
         const daysUntilClose = parseInt(core.getInput('days-until-close', { required: true }));
         const triggerLabel = core.getInput('trigger-label', { required: true });
         const closingComment = core.getInput('closing-comment', { required: true });
+        core.info(`Running with params ${daysUntilClose} ${triggerLabel} ${closingComment}.`);
         const client = github.getOctokit(token);
         const issues = await client.rest.issues.list();
-        core.info(`Found ${issues.data.length} issues ${daysUntilClose} ${triggerLabel} ${closingComment}.`);
+        core.info(`Found ${issues.data.length} issues.`);
         core.debug(new Date().toTimeString());
     }
     catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,11 @@ async function run(): Promise<void> {
 		const daysUntilClose = parseInt(core.getInput('days-until-close', {required: true}));
 		const triggerLabel = core.getInput('trigger-label', {required: true});
 		const closingComment = core.getInput('closing-comment', {required: true});
+		core.info(`Running with params ${daysUntilClose} ${triggerLabel} ${closingComment}.`);
 
 		const client = github.getOctokit(token);
 		const issues = await client.rest.issues.list();
-		core.info(`Found ${issues.data.length} issues ${daysUntilClose} ${triggerLabel} ${closingComment}.`);
+		core.info(`Found ${issues.data.length} issues.`);
 
 		core.debug(new Date().toTimeString());
 	} catch (error) {

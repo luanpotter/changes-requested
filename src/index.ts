02ba070deacb0f231ce923d0581d021fb2f4bf6e
Luan Nico <luanpotter27@gmail.com>
2021-05-15 14:12:31 -0400

    .

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ async function run(): Promise<void> {
 		core.info(`Running with params ${daysUntilClose} ${triggerLabel} ${closingComment}.`);
 
 		const client = github.getOctokit(token);
-		const issues = await client.rest.issues.list();
+		core.info(`Client: ${client}.`);
+		const issues = await client.rest.issues.list({baseUrl: 'luanpotter/changes-requested'});
+		core.info(`Client: ${issues.status}.`);
 		core.info(`Found ${issues.data.length} issues.`);
 
 		core.debug(new Date().toTimeString());
